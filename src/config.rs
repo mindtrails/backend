4f4c3df6449720b5e0e9bd120e409d9a483bbf8c
George Dragomir <george.dragomir.x@gmail.com>
2022-12-09 17:36:49 +0200

    Add a postgres connection

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,13 @@
 use thiserror::Error;
 
+const FALLBACK_POSTGRES_URL: &str = "postgres://postgres:postgres@localhost/mindtrails";
 const FALLBACK_PORT: u16 = 3000;
 
 #[derive(Debug)]
 pub struct Config
 {
+    postgres_url: String,
+
     port: u16,
 }
 
@@ -12,13 +15,24 @@ impl Config
 {
     pub fn init() -> Result<Self>
     {
+        let postgres_url = match ::std::env::var("POSTGRES_URL") {
+            Ok(url) => url,
+            Err(::std::env::VarError::NotPresent) => String::from(FALLBACK_POSTGRES_URL),
+            Err(err) => Err(err)?,
+        };
+
         let port = match ::std::env::var("PORT") {
             Ok(port) => port.parse()?,
             Err(::std::env::VarError::NotPresent) => FALLBACK_PORT,
             Err(err) => Err(err)?,
         };
 
-        Ok(Config { port })
+        Ok(Config { postgres_url, port })
+    }
+
+    pub fn postgres_url(&self) -> &str
+    {
+        &self.postgres_url
     }
 
     pub fn port(&self) -> u16
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
+use std::{net::SocketAddr, time::Duration};
+
+use axum::{extract::State, http::status::StatusCode, routing::get, Router, Server};
+use sqlx::{self, postgres::PgPoolOptions, PgPool};
+
 use thiserror::Error;
+
+use mindtrails_backend::config::{self, Config};
 
 #[tokio::main]
 async fn main() -> Result<(), self::Error>
 {
-    let config = mindtrails_backend::config::Config::init()?;
+    let config = Config::init()?;
 
-    let app = axum::Router::new().route("/", axum::routing::get(root));
-
-    let addr = ::std::net::SocketAddr::from(([127, 0, 0, 1], config.port()));
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    let pg_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(config.postgres_url())
         .await?;
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.port()));
+
+    let app = Router::new().route("/", get(root)).with_state(pg_pool);
+
+    Server::bind(&addr).serve(app.into_make_service()).await?;
 
     Ok(())
 }
 
-async fn root() -> &'static str
+async fn root(State(pg_pool): State<PgPool>) -> Result<String, (StatusCode, String)>
 {
-    "Hello, world!"
-}
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(256_i64)
+        .fetch_one(&pg_pool)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
 
-use mindtrails_backend::config;
+    Ok(row.0.to_string())
+}
 
 #[derive(Debug, Error)]
 enum Error
 {
     #[error("{0}")]
     Config(#[from] config::Error),
+    #[error("{0}")]
+    Sqlx(#[from] sqlx::Error),
     #[error("{0}")]
     Hyper(#[from] hyper::Error),
 }
