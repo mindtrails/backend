c3942b928154ed03fc1321caacc3d7f7c624e589
George Dragomir <george.dragomir.x@gmail.com>
2022-12-09 17:18:47 +0200

    Add proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use thiserror::Error;
+
 #[tokio::main]
-async fn main()
+async fn main() -> Result<(), self::Error>
 {
     let app = axum::Router::new().route("/", axum::routing::get(root));
 
@@ -7,11 +9,19 @@ async fn main()
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap()
+        .await?;
+
+    Ok(())
 }
 
 async fn root() -> &'static str
 {
     "Hello, world!"
+}
+
+#[derive(Debug, Error)]
+enum Error
+{
+    #[error("{0}")]
+    Hyper(#[from] hyper::Error),
 }
